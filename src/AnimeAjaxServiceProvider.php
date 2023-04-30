73c263f6a2bdc63b97457b525b836ef930676158
TanNhatCMS <mrkiet.edu@gmail.com>
2023-05-01 02:46:05 +0700

    fix router

--- src/AnimeAjaxServiceProvider.php
+++ src/AnimeAjaxServiceProvider.php
@@ -6,12 +6,56 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AnimeAjaxServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-    }
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Where the route file lives, both inside the package and in the app (if overwritten).
+     *
+     * @var string
+     */
+    public $routeFilePath = '/routes/pagemanager.php';
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+
     public function boot()
     {
         //$this->loadViewsFrom(__DIR__ . '/../resources/views/ajax', 'ajax');
     }
+    /**
+     * Define the routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function setupRoutes(Router $router)
+    {
+        // by default, use the routes file provided in vendor
+        $routeFilePathInUse = __DIR__.$this->routeFilePath;
 
+        // but if there's a file with the same name in routes/backpack, use that one
+        if (file_exists(base_path().$this->routeFilePath)) {
+            $routeFilePathInUse = base_path().$this->routeFilePath;
+        }
+
+        $this->loadRoutesFrom($routeFilePathInUse);
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->setupRoutes($this->app->router);
+    }
 }
